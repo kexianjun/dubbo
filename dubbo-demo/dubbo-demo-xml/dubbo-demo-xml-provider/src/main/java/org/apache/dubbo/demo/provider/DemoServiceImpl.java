ab823d34c380d18907df655459a50bdc029c79f6
kexianjun <kexianjun1991@hotmail.com>
2019-04-01 13:31:47 +0800

    fast json test

--- dubbo-demo/dubbo-demo-xml/dubbo-demo-xml-provider/src/main/java/org/apache/dubbo/demo/provider/DemoServiceImpl.java
+++ dubbo-demo/dubbo-demo-xml/dubbo-demo-xml-provider/src/main/java/org/apache/dubbo/demo/provider/DemoServiceImpl.java
@@ -17,10 +17,18 @@
 package org.apache.dubbo.demo.provider;
 
 import org.apache.dubbo.demo.DemoService;
+import org.apache.dubbo.demo.PersonTest;
 import org.apache.dubbo.rpc.RpcContext;
 
+import com.alibaba.fastjson.JSON;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DemoServiceImpl implements DemoService {
     private static final Logger logger = LoggerFactory.getLogger(DemoServiceImpl.class);
@@ -30,5 +38,50 @@ public class DemoServiceImpl implements DemoService {
         logger.info("Hello " + name + ", request from consumer: " + RpcContext.getContext().getRemoteAddress());
         return "Hello " + name + ", response from provider: " + RpcContext.getContext().getLocalAddress();
     }
+
+    @Override
+    public Map<String, List<PersonTest<String>>> sayHi(String name) {
+        Map<String, List<PersonTest<String>>> map = new HashMap<>();
+        Stream.of(1, 2, 3).forEach(i -> {
+            List<PersonTest<String>> personTestList = Stream.of("a", "b", "c").map(c -> {
+                PersonTest<String> personTest = new PersonTest<>();
+                personTest.setName("name:" + c);
+                personTest.setAge("haha");
+                return personTest;
+            }).collect(Collectors.toList());
+            map.put(i + ":" + name, personTestList);
+        });
+        return map;
+    }
+
+    @Override
+    public String fastjsonTest1(PersonTest<String> personTest) {
+        System.out.println(personTest.getName());
+        System.out.println(personTest.getAge());
+        return "PersonTest<String>";
+    }
+
+    @Override
+    public String fastjsonTest1(List<PersonTest<String>> list) {
+        System.out.println("receive");
+        list.forEach(person -> {
+            System.out.println(person.getAge());
+            System.out.println(person.getName());
+        });
+        return "List<PersonTest<String>>";
+    }
+
+    @Override
+    public String fastjsonTest1(Map<String, List<PersonTest<String>>> listMap) {
+        listMap.forEach((key, value) -> {
+            System.out.println(key);
+            value.forEach(person -> {
+                System.out.println(person.getAge());
+                System.out.println(person.getName());
+            });
+        });
+        return "hello";
+    }
+
 
 }
